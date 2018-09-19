b150350a4b630bf85dff6d65182d699662ef92fa
Daniel Kirch <daniel.kirch@2trde.com>
2018-09-19 11:08:16 +0200

    Renamed to amqpy (maybe more pythonic?)

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='amqp_python',
+setup(name='amqpy',
       version='0.1',
       description='Simple amqp endpoint for python, based on pika',
       url='http://github.com/2trde/amqp_dsl',
       author='Daniel Kirch',
       author_email='daniel.kirch@2trde.com',
-      packages=['amqp_python'],
+      packages=['amqpy'],
       install_requires=[
         'pika'
       ])
