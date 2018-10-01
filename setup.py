2bdf79ddf6ef96235b28e714550a93e1df20fb49
Daniel Kirch <daniel.kirch@2trde.com>
2018-10-01 13:46:20 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='amqpy',
-      version='0.1',
+      version='0.2',
       description='Simple amqp endpoint for python, based on pika',
       url='http://github.com/2trde/amqp_dsl',
       author='Daniel Kirch',
